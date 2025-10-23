069cd3a0aa28ca6999c0376cb9b041379a0f7b42
fayfaybafateleo <fayfaybafateleo@gmail.com>
2025-10-24 04:58:59 +0800

    Gunsound/MuzzleFlash Adujustment

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using Unity.Cinemachine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+using UnityEditor.PackageManager;
 
 public class GunScript : MonoBehaviour
 {
@@ -25,17 +27,56 @@ public class GunScript : MonoBehaviour
     public Animator GunAnimator;
     public float AnimatorSpeed;
     public GunShake shake;
+
+
+    [Header("ZeroTheFirePoint")]
+    public Camera Camera;
+    public float MaxDistance;
+    public LayerMask HitLayers = ~0;         
+    public LayerMask IgnoreLayers = 0;   
+    public float Offset = 0.1f;          
     // Slug Count//
     public int SlugCount;
 
     public GameObject GunModel;
     public GameObject MuzzleFlash;
     public WeaponWaging WeaponWagingScript;
-    
-  
+
+
     // Update is called once per frame
+    void Start()
+    {
+        Camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
     void Update()
     {
+        //ZeroTheFirePoint
+        Vector3 RayOrigin = Camera.transform.position + Camera.transform.forward * Offset;
+        Vector3 RayDir = Camera.transform.forward;
+
+        int mask = HitLayers;
+        if (IgnoreLayers.value != 0)
+        {
+          mask = HitLayers & ~IgnoreLayers;
+        }
+        Vector3 EndPoint;
+        if (Physics.Raycast(RayOrigin, RayDir, out RaycastHit hit, MaxDistance, mask, QueryTriggerInteraction.Ignore))
+        {
+            EndPoint = hit.point;
+        }
+        else
+        {
+            EndPoint = RayOrigin + RayDir * MaxDistance;
+        }
+        Vector3 DirFromMuzzle = (EndPoint - FirePoint.position).normalized;
+
+        // 3) 应用（带可选的固定偏置角，修正模型轴）
+        Quaternion aim = Quaternion.LookRotation(DirFromMuzzle, Vector3.up);
+        FirePoint.rotation = aim;
+
+        Debug.DrawRay(RayOrigin, RayDir * MaxDistance, Color.cyan);             // 相机射线
+        Debug.DrawRay(FirePoint.position, DirFromMuzzle * MaxDistance, Color.red); // 枪口方向
+
         ReloadTimer -= Time.deltaTime;
        GunAnimator.speed = AnimatorSpeed;
         // Mouse pressed
