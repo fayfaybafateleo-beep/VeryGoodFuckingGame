32023ab7f700bea592b0b71d8bd11bea0b0fc4ac
fayfaybafateleo <fayfaybafateleo@gmail.com>
2025-10-31 01:08:35 +0800

    explosion setup

---
+++ Assets/Scripts/Explosive.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+public class Explosive : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   
+
+    void Start()
+    {
+        
+    }
+
+  
+}
---
+++ Assets/Scripts/GLauncher.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class GLauncher : MonoBehaviour
+{
+    [Header("WayToDetonate")]
+    public bool IsPercussion;
+    public float FuzeTime = 3f;
+    public float FuzeTimer = 0f;
+
+    [Header("ExplosionPrefab")]
+    public GameObject Explosive;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
