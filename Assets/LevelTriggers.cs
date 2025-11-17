f771e156d7bcdc08e0094a4fe97c2bd0119e6144
fayfaybafateleo <fayfaybafateleo@gmail.com>
2025-11-17 17:38:42 +0800

    Level Update

---
+++ Assets/LevelTriggers.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class LevelTriggers : MonoBehaviour
+{
+    [Header("StateOfTrigger")]
+    public bool IsEnd;
+    public bool IsTriggered;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            IsTriggered = true;
+        }
+    }
+}
---
+++ Assets/Scripts/GameLevelControl.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class GameLevelControl : MonoBehaviour
+{
+    [Header("TriggerZone")]
+    public GameObject StartingPoint;
+    public GameObject EndingPoint;
+    public LevelTriggers StartTrigger;
+    public LevelTriggers EndTrigger;
+
+    [Header("Bools")]
+    public bool IsActive;
+    public enum LevelState
+    { 
+        NotActive,
+        Active,
+        End
+    }
+    public LevelState LS;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        switch (LS) 
+        {
+            case LevelState.NotActive:
+                IsActive = false;
+
+                if (StartTrigger.IsTriggered)
+                {
+                    LS = LevelState.Active;
+                }
+                break;
+            case LevelState.Active:
+                IsActive = true;
+
+                if (EndTrigger.IsTriggered)
+                {
+                    LS = LevelState.End;
+                }
+                break;
+            case LevelState.End:
+
+                break;
+        }
+
+    }
+
+}
