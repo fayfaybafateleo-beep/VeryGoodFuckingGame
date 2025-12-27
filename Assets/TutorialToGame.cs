4c9b58b120325771a08750808620115b8929f18e
fayfaybafateleo-beep <fayfaybafateleo@gmail.com>
2025-12-28 05:26:45 +0800

    TutorialUpdate

---
+++ Assets/TutorialToGame.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialToGame : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SwapToGame();
+        }
+    }
+
+    public void SwapToGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
