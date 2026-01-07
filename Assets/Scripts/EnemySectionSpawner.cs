fb99af3f60f9713eb7c2112aeae7a30b1074619c
fayfaybafateleo-beep <fayfaybafateleo@gmail.com>
2026-01-07 08:32:45 +0800

    New SpawnSystem

--- Assets/Scripts/EnemySectionSpawner.cs
+++ Assets/Scripts/EnemySectionSpawner.cs
@@ -8,16 +8,30 @@ public class EnemySectionSpawner : MonoBehaviour
     public SectionManager.SectionType Section;
     public SectionManager SectionManager;
 
-    [Header("À¢π÷…Ë÷√")]
-    public List<GameObject> ObjectList = new List<GameObject>();  
+    [Header("Spawner")]
+    public List<GameObject> ObjectList = new List<GameObject>();
+    public List<GameObject> EliteList = new List<GameObject>();
+    public List<GameObject> MiniBossList = new List<GameObject>();
+
+
     public List<Transform> SpawnPositions = new List<Transform>(); 
     public float SpawnInterval = 2f;   
-    public int MaxSpawnInThisSection = 10; 
+    public int MaxSpawnInThisSection = 10;
+
+
+    public int MaxEliteInThisSection = 2;
+    public int MaxMiniBossInThisSection = 1;
+
+    public int LocalEliteCount = 0;
+    public int LocalMiniBossCount = 0;
 
     private float SpawnTimer = 0f;
     private int LocalSpawnCount = 0;
 
+    public AnimationCurve EliteChance = AnimationCurve.Linear(0, 0f, 10, 0.35f); 
+    public AnimationCurve MiniBossChance = AnimationCurve.Linear(0, 0f, 10, 0.08f);
 
+    public int DangerLevel;
     private void Start()
     {
       
@@ -60,8 +74,10 @@ public class EnemySectionSpawner : MonoBehaviour
     {
         if (ObjectList.Count == 0) return;
 
+        GameObject prefab = PickPrefabByDanger();
+        if (prefab == null) return;
+
         Vector2 offset = Random.insideUnitCircle * 1f;
-        GameObject prefab = ObjectList[Random.Range(0, ObjectList.Count)];
 
         Transform spawnPoint = transform;
         Vector3 offsetPoisition = spawnPoint.position;
@@ -84,5 +100,33 @@ public class EnemySectionSpawner : MonoBehaviour
         LocalSpawnCount = 0;
         SpawnTimer = 0f;
      
+    }
+
+    public GameObject PickPrefabByDanger()
+    {
+        //MiniBoss
+        if (MiniBossList.Count > 0 && LocalMiniBossCount < MaxMiniBossInThisSection)
+        {
+            float miniBossChance = Mathf.Clamp01(MiniBossChance.Evaluate(DangerLevel));
+            if (Random.value < miniBossChance)
+            {
+                LocalMiniBossCount++;
+                return MiniBossList[Random.Range(0, MiniBossList.Count)];
+            }
+        }
+
+        // Elite
+        if (EliteList.Count > 0 && LocalEliteCount < MaxEliteInThisSection)
+        {
+            float eliteChance = Mathf.Clamp01(EliteChance.Evaluate(DangerLevel));
+            if (Random.value < eliteChance)
+            {
+                LocalEliteCount++;
+                return EliteList[Random.Range(0, EliteList.Count)];
+            }
+        }
+
+        // NormalMob
+        return ObjectList[Random.Range(0, ObjectList.Count)];
     }
 }
