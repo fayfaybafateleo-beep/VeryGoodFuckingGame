3d17c3b53f5c649b2cee6aa917493b349528a1ca
fayfaybafateleo-beep <fayfaybafateleo@gmail.com>
2026-01-09 22:34:47 +0800

    EnemyAdjust

--- Assets/DangerLevel.cs
+++ Assets/DangerLevel.cs
@@ -5,16 +5,21 @@ public class DangerLevel : MonoBehaviour
     [Header("DangerLevel")]
     public static int GameDangerLevel;
 
- 
+    [Header("DeveloperTool")]
+    public bool Enable=false;
+    public KeyCode Key=KeyCode.L;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GameDangerLevel = 10;
+     
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(Key)&& Enable)
+        {
+            GameDangerLevel += 1;
+        }
     }
 }
